e53334e436c713bab4d77656002750dce4427a91
Artiely <1119696785@qq.com>
2018-05-05 19:50:44 +0800

    修改路径

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const { resolve } = require('path')
 const server = require('http').Server(app)
 
-app.use('/', express.static(resolve(__dirname, '../dist')))
+app.use('/', express.static(resolve(__dirname, './.vuepress/dist')))
 
 server.listen(8085, function() {
   console.log('Node app start at http://localhost:8085')
