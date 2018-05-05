728b53af64c90a25dae4412a58627e69e5b7847b
Artiely <1119696785@qq.com>
2018-05-05 18:34:38 +0800

    repo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,24 @@
 module.exports = {
   title: '为了爱与和平',
   description: 'for love and peace',
+  port: '8085',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
       { text: 'External', link: 'https://google.com' }
-    ]
+    ],
+    // 假定 GitHub。也可以是一个完整的 GitLab 网址
+    repo: 'artiely/chebian-docs',
+    repoLabel: '查看源码',
+    // 如果你的文档不在仓库的根部
+    // docsDir: 'docs',
+    // 可选，默认为 master
+    docsBranch: 'master',
+    // 默认为 true，设置为 false 来禁用
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'github上编辑此页'
   },
   themeConfig: {
     locales: {
