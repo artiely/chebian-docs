89a78dcbb5ba6cbc39f6161a9c670ab80131361d
artiely <1119696785@qq.com>
2019-06-06 17:33:02 +0800

    更新

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const { resolve } = require('path')
 const server = require('http').Server(app)
-
+var localhost = require('address').ip()
+var opn = require('opn')
 app.use('/', express.static(resolve(__dirname, './.vuepress/dist')))
 
-server.listen(8086, function() {
-  console.log('Node app start at http://localhost:8086')
+// server.listen(8086, function() {
+//   console.log('Node app start at http://localhost:8086')
+// })
+const port = 8086
+server.listen(port, () => {
+  var uri = `http://${localhost}:${port}`
+  console.log(uri)
+  opn(uri)
 })
