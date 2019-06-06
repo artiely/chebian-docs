dd0b9f9f762538deab120c9c896e962b194c2b3e
artiely <1119696785@qq.com>
2019-06-06 15:19:27 +0800

    all

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       // pre-deploy action
       'pre-deploy-local': "git fetch --all",
       // post-deploy action
-      'post-deploy': "npm install && pm2 startOrRestart ecosystem.config.js --env production",
+      'post-deploy': "pm2 startOrRestart ecosystem.config.js --env production",
     },
   }
 }
